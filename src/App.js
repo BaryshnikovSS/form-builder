f3a414651c92025625c33759a84ff2ce43a9fc9a
SEBAR <baryshnikov.serhii@gmail.com>
2024-09-19 16:37:06 +0300

    feat(INIT): routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { FormRenderer, FormGenerator } from './pages';
+import { Layout } from './components';
+import { ROUTES } from './constants';
 
-function App() {
+const Routing = () => (
+  <>
+    <Routes>
+      <Route
+        path={ROUTES.ROOT}
+        element={<Navigate to={ROUTES.FORM_GENERATOR} />}
+      />
+      <Route
+        path={ROUTES.FORM_GENERATOR}
+        element={<FormGenerator />}
+      />
+      <Route
+        path={ROUTES.FORM_RENDERER}
+        element={<FormRenderer />}
+      />
+    </Routes>
+  </>
+);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <div className='App'>
+          <Routing />
+        </div>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
